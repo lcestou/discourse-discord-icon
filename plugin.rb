18fff09dc882f0a663c6314bba04029856614231
Luis Cestou <maisto@gmail.com>
2019-06-18 03:42:33 -0400

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-discord-icon
+# about: Register fab-discord SVG Icon
+# version: 0.1
+# authors: Luis Cestou
+# url: https://github.com/lcestou/discourse-discord-icon
+
+register_svg_icon "fab-discord" if respond_to?(:register_svg_icon)
